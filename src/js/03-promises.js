b84c302bbd82c70b93b7322e2e1eb4cace4ccf56
NPC-Roman <ispacebobo@gmail.com>
2023-08-20 12:39:16 +0300

    Change5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+/*
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +6,41 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
-}
+}*/
+
+const Notiflix = require('notiflix'); // CommonJS import
+import Notiflix from 'notiflix'; // ES6 import
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Останній код з попередньої відповіді
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const delayInput = form.querySelector('[name="delay"]');
+    const stepInput = form.querySelector('[name="step"]');
+    const amountInput = form.querySelector('[name="amount"]');
+
+    const delay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+
+      delay += step;
+    }
+  });
+
+  // Останній код з попередньої відповіді
+});
