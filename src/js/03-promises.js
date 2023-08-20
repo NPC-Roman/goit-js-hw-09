9a8d140bd6d73368f753fea1990a1637bad530b7
NPC-Roman <ispacebobo@gmail.com>
2023-08-20 13:01:29 +0300

    Change6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,40 +7,41 @@ function createPromise(position, delay) {
     // Reject
   }
 }*/
+import Notiflix from 'notiflix';
+const Notiflix = require('notiflix');
 
-const Notiflix = require('notiflix'); // CommonJS import
-import Notiflix from 'notiflix'; // ES6 import
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Останній код з попередньої відповіді
-
-  form.addEventListener('submit', async event => {
-    event.preventDefault();
-
-    const delayInput = form.querySelector('[name="delay"]');
-    const stepInput = form.querySelector('[name="step"]');
-    const amountInput = form.querySelector('[name="amount"]');
-
-    const delay = parseInt(delayInput.value);
-    const step = parseInt(stepInput.value);
-    const amount = parseInt(amountInput.value);
-
-    for (let i = 1; i <= amount; i++) {
-      createPromise(i, delay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(
-            `✅ Fulfilled promise ${position} in ${delay}ms`
-          );
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(
-            `❌ Rejected promise ${position} in ${delay}ms`
-          );
-        });
-
-      delay += step;
-    }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
 
-  // Останній код з попередньої відповіді
+document.querySelector('.form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const delayInput = parseInt(e.target.elements.delay.value);
+  const stepInput = parseInt(e.target.elements.step.value);
+  const amountInput = parseInt(e.target.elements.amount.value);
+
+  for (let i = 0; i < amountInput; i++) {
+    const currentDelay = delayInput + i * stepInput;
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.Success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.Failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
 });
